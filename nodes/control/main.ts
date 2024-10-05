1875da1b379cc162e60d2d932b05172dc7d4dce2
Forrest <light24bulbs@gmail.com>
2024-10-05 13:43:14 -0700

    fin test

--- nodes/control/main.ts
+++ nodes/control/main.ts
@@ -125,9 +125,9 @@ class ActuatorNode {
 
   setFin(fin: Fin, angle: number) {
     // Limit the angle to ±40 degrees
-    angle = Math.max(-70, Math.min(70, angle));
+    angle = Math.max(-40, Math.min(40, angle));
     //calculate 1000-2000 pulse length 1000 is 0 degrees, 2000 is 180 degrees
-    const pulse = 1500 + (angle / 90) * 500;
+    const pulse = 1500 + (angle / 90) * 1000;
     // const dutyCycle = 50 + (angle / 180) * 50;
     const channel = chan[fin];
     this.pwm.setPulseLength(channel, pulse);
@@ -155,14 +155,14 @@ class ActuatorNode {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    for (let i = -70; i <= 70; i++) {
+    for (let i = -40; i <= 40; i++) {
         ['portStab','topRud','starbStab','bottomRud'].forEach((fin) => {
           this.setFin(fin as Fin, i);
       })
       // this.logI2CRegisters();
       await sleep(20);
     }
-    for (let i = 70; i >= -70; i--) {
+    for (let i = 40; i >= -40; i--) {
       ['portStab','topRud','starbStab','bottomRud'].forEach((fin) => {
         this.setFin(fin as Fin, i);
       })
